92b84a69f78647dc6a5e96826bc1d6789b48a956
s-heppner <mail@s-heppner.com>
2024-06-16 09:35:36 +0200

    Remove rofi configuration, as I do not use it anymore

--- deploy_scripts/deploy_fedora_workstation.sh
+++ deploy_scripts/deploy_fedora_workstation.sh
@@ -25,8 +25,6 @@ dconf load /org/gnome/terminal/legacy/profiles:/ < ${DOTFILE_DIR}/gnome-terminal
 # Bash
 ./symlink_dotfile.sh ${DOTFILE_DIR}/home/.bashrc ${HOME}/.bashrc
 ./symlink_dotfile.sh ${DOTFILE_DIR}/home/.bashrc_scripts ${HOME}/.bashrc_scripts
-# Rofi
-./symlink_dotfile.sh ${DOTFILE_DIR}/home/.config/rofi ${HOME}/.config/rofi
 
 # TLP Power Management
 # SELinux does not like it if this is a symbolic link 
