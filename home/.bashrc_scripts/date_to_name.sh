f019612b0d8f32630851477db154ec42001cb5a9
s-heppner <mail@s-heppner.com>
2025-04-03 20:22:18 +0200

    .bashrc_scripts/date_to_name.sh: Fix datetime_to_filename method
    Previously, the `datetime_to_filename` method missed to add
    seconds to the filename, which could lead to duplicate times, for
    example when having taken several pictures in one minute.
    
    This adds the seconds to the filename and furthemore removes the
    colons between the hour, minute and seconds of the ISO-8601 format.
    While this is not exactly standard-conform, it is the only way the
    filenames are allowed on Windows machines, which I sadly cannot
    get around fully at this moment.
    
    Fixes #13

--- home/.bashrc_scripts/date_to_name.sh
+++ home/.bashrc_scripts/date_to_name.sh
@@ -6,6 +6,8 @@
 
 date_to_filename() {
     if [ "$#" -eq 0 ]; then
+        echo "Adds an isoformat date to the specified file(s) based on their last modification."
+        echo "Format: YYYY-mm-dd"
         echo "Usage: date_to_filename <path or wildcard>"
         return 1
     fi
@@ -25,14 +27,21 @@ date_to_filename() {
 
 
 datetime_to_filename() {
+    # (2025-04-03, s-heppner)
+    # I decided to remove the colons (:) between the hour, minute and 
+    # seconds of the ISO-8601 format. While this is not exactly 
+    # standard-conform, it is the only way the filenames are valid on 
+    # Windows machines, which I sadly cannot get around fully at this moment.
     if [ "$#" -eq 0 ]; then
+        echo "Adds an isoformat datetime to the specified file(s) based on their last modification."
+        echo "Format: YYYY-mm-ddTHHMMSS"
         echo "Usage: datetime_to_filename <path or wildcard>"
         return 1
     fi
 
     for file in "$@"; do
         if [ -f "$file" ]; then
-            mod_date=$(date -r "$file" +%Y-%m-%dT%H:%M)
+            mod_date=$(date -r "$file" +%Y-%m-%dT%H%M%S)
             dir=$(dirname "$file")
             base=$(basename "$file")
             mv "$file" "$dir/$mod_date $base"
